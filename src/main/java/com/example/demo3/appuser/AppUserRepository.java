2adae0c2f857beeed843aa910865f563224d27df
ryanmj14 <ryanjones1417@gmail.com>
2023-10-28 11:07:45 -0400

    Adding appuser

---
+++ src/main/java/com/example/demo3/appuser/AppUserRepository.java
@@ -0,0 +1,24 @@
+package com.example.demo3.appuser;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByEmail(String email);
+    Optional<AppUser> findById(Long id);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE AppUser a " +
+            "SET a.enabled = TRUE WHERE a.email = ?1")
+    int enableAppUser(String email);
+	
+}
